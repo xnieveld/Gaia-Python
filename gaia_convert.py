08de282b0c4d0c217d9b6dd8755a540c80e23867
danny <dannydorstijn1997@gmail.com>
2019-01-22 22:56:39 +0100

    small cleanup

--- gaia_convert.py
+++ gaia_convert.py
@@ -24,110 +24,71 @@ GAIA_PMRA = GAIADATASETS.pmra
 GAIA_PMDEC = GAIADATASETS.pmdec
 GAIA_RADIAL_VELOCITY = GAIADATASETS.radial_velocity.dropna()
 
-# Hoek omrekenen
-TAN = (GAIA_PAR / 1000 / 60 / 60 / 180)
-RAW_D = (2 * 1.511 * 10 ** 11) / np.tan(TAN * math.pi)
+DISTANCE = 1.0 / GAIA_PAR * 1000
 
 # print(lengte_breedte)
 # merge = pd.DataFrame({'ra': GAIA_RA, 'dec': GAIA_DEC})
+print(DESIGNATION)
+
 
 def brightness():
     ''' Berekent helderheid ster '''
 
     # print(gaia_par)
-    # print(RAW_D)
+    # print(DISTANCE)
     # distance modulus omrekenen
     # reminder t = 10 ** 4
 
     # Absolute Magnitude
     absolute_magnitude = GAIA_PHOT_G_MEAN_MAG - 5 * \
-    np.log10((RAW_D / (3.08567758149137 * 10 ** 16) / 10))
+        np.log10((DISTANCE / (3.08567758149137 * 10 ** 16) / 10))
     # Hier wordt bekeken hoeveel rijen geen data hebben.
     # absmag_columns = ['designation', 'abs_mag_conv']
     #stops = pd.DataFrame(data, columns=absmag_columns)
-    abs_empty_value = pd.DataFrame({'aantal_lege_magnitudes': absolute_magnitude})
+    abs_empty_value = pd.DataFrame(
+        {'aantal_lege_magnitudes': absolute_magnitude})
     print(abs_empty_value.isnull().sum().sum())
 
     # NaN wordt uit de lijst gehaald.
     absolute = absolute_magnitude.dropna()
-    abs_data = pd.DataFrame({'designation': DESIGNATION, 'sterBright': absolute})
-    # De helderheid van de sterren worden geformatteerd in een decimaal
-    absolute_data = (abs_data.round({'designation': 0, 'sterBright': 1})).dropna()
-    print(absolute_data)
+
+    # Star uuid converted to unsigned int64
+    # STAR_ID = np.uint64(DESIGNATION.split()[-1])
+    # abs_data = pd.DataFrame(
+    #     {'designation': STAR_ID, 'sterBright': absolute})
+    # # De helderheid van de sterren worden geformatteerd in een decimaal
+    # absolute_data = abs_data.dropna()
+    # print(absolute_data)
 
     # en file wegschrijven naar JSON
     absolute_data.to_json("data/abs_magnitude.json", orient="records")
 
+
 def convert_xyz():
     ''' Converts Gaia data to XYZ values '''
-    polar_x = GAIA_RA
-    polar_y = GAIA_DEC
-    polar_z = RAW_D
-
-    gaia_x = np.cos(polar_x) * np.sin(polar_y)
-    gaia_y = np.cos(polar_y)
-    gaia_z = np.sin(polar_x) * np.sin(polar_y)
-
-    cartesian = (gaia_x * polar_z + gaia_y * polar_z + gaia_z * polar_z)
-    # center = 0.0
-    cart_nan = (cartesian + 0.0).dropna()
-
-    cartesian = pd.DataFrame({'cartesian': cart_nan}).round(1)
-    print(cartesian)
-
-    cartesian.to_json("data/Cartesian.json", orient="records")
-
-    var_ra = GAIA_RA.round(1)
-    dec = GAIA_DEC.round(1)
-    distance = RAW_D
-
-    distance = distance.round(1)
-    print(distance)
 
     # star_position = (ra, dec, distance)
+    star_pos_x = np.float64(DISTANCE * np.cos(GAIA_RA) * np.sin(GAIA_DEC))
+    star_pos_y = np.float64(DISTANCE * np.cos(GAIA_DEC))
+    star_pos_z = np.float64(DISTANCE * np.sin(GAIA_RA) * np.sin(GAIA_DEC))
 
-    ster_pos_x = np.cos(var_ra) * np.sin(dec)
-    ster_pos_y = np.cos(dec)
-    ster_pos_z = np.sin(var_ra) * np.sin(dec)
+    # Star uuid converted to unsigned int64
+    STAR_ID = np.uint64(DESIGNATION.split()[-1])
+    print(STAR_ID)
 
-    ster_pos_x = ster_pos_x.round(2)
-    print(ster_pos_x)
+    star_pos = pd.DataFrame({'designation': STAR_ID, 'starX': star_pos_x,
+                             'starY': star_pos_y, 'starZ': star_pos_z})
 
-    ster_pos_y = ster_pos_y.round(2)
-    print(ster_pos_y)
+    star_pos.to_json("data/sterXYZ.json", orient="records")
 
-    ster_pos_z = ster_pos_z.round(2)
-    print(ster_pos_z)
-
-    sterpositie = pd.DataFrame({'designation': DESIGNATION, 'sterX': ster_pos_x,
-                                'sterY': ster_pos_y, 'sterZ': ster_pos_z})
-    print(sterpositie)
-
-    sterpositie.to_json("data/sterXYZ.json", orient="records")
-
-    ster_positie = (ster_pos_x * distance + ster_pos_y *
-                    distance + ster_pos_z * distance)
-
-    gaia_ster_a = ster_positie.round(1)
-    print(gaia_ster_a)
-
-    velocity_x = np.cos(GAIA_PMRA + var_ra) + np.sin(GAIA_PMDEC + dec)
-    velocity_y = np.cos(GAIA_PMDEC + dec)
-    velocity_z = np.sin(GAIA_PMRA + var_ra) * np.sin(GAIA_PMDEC + dec)
-    velocity = (velocity_x * (distance + GAIA_RADIAL_VELOCITY) + velocity_y *
-                (distance + GAIA_RADIAL_VELOCITY) + velocity_z * (distance + GAIA_RADIAL_VELOCITY))
-
-    gaia_ster_b = ster_positie + velocity
-
-    velocity = (gaia_ster_b - gaia_ster_a)
-
-    # velocity_column = ['Velocity': velocity]
-    velocity = velocity.dropna()
-    velocity = pd.DataFrame({'velocity': velocity})
-    velocity = velocity.astype('double')
-    print(velocity)
     # end xyz
     # G = whitelight, GBp = blue, and GRp = red
+
+
+def calculate_velocity():
+    """ Calculate velocity """
+    pass
+
 
 brightness()
 convert_xyz()
